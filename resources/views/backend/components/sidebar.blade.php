9b72a1965f52ea69b4ce7ec4c540d1b44f1d8270
dodinovembri <dodinovembri32@gmail.com>
2021-02-14 22:04:59 +0700

    update

---
+++ resources/views/backend/components/sidebar.blade.php
@@ -0,0 +1,179 @@
+    <div class="sidebar">
+      <div class="sidebar-header">
+        <div>
+          <a href="../index.html" class="sidebar-logo"><span>cassie</span></a>
+          <small class="sidebar-logo-headline">Responsive Dashboard Template</small>
+        </div>
+      </div><!-- sidebar-header -->
+      <div id="dpSidebarBody" class="sidebar-body">
+        <ul class="nav nav-sidebar">
+          <li class="nav-label"><label class="content-label">Setting</label></li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="book"></i> System</a>
+            <nav class="nav nav-sub">
+              <a href="{{ url('home-index') }}" class="nav-sub-link">Home Index</a>
+              <a href="../components/con-icons.html" class="nav-sub-link">Icons</a>
+              <a href="../components/con-images.html" class="nav-sub-link">Images</a>
+              <a href="../components/con-typography.html" class="nav-sub-link">Typography</a>
+            </nav>
+          </li>           
+          <li class="nav-label"><label class="content-label">Template Pages</label></li>
+          <li class="nav-item show">
+            <a href="" class="nav-link with-sub active"><i data-feather="box"></i> Dashboard</a>
+            <ul class="nav nav-sub">
+              <li class="nav-sub-item"><a href="dashboard-one.html" class="nav-sub-link active">Analytics &amp; Monitoring</a></li>
+              <li class="nav-sub-item"><a href="dashboard-two.html" class="nav-sub-link">Projects &amp; Web Services</a></li>
+              <li class="nav-sub-item"><a href="dashboard-three.html" class="nav-sub-link">Blog &amp; Social Media</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="layout"></i> App Pages</a>
+            <nav class="nav nav-sub">
+              <a href="app-calendar.html" class="nav-sub-link">Calendar</a>
+              <a href="app-chat.html" class="nav-sub-link">Chat</a>
+              <a href="app-mail.html" class="nav-sub-link">Mail</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="lock"></i> Authentication</a>
+            <nav class="nav nav-sub">
+              <a href="page-signin.html" class="nav-sub-link">Sign In</a>
+              <a href="page-signup.html" class="nav-sub-link">Sign Up</a>
+              <a href="page-forgot.html" class="nav-sub-link">Forgot Password</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="x-circle"></i> Error Pages</a>
+            <nav class="nav nav-sub">
+              <a href="page-404.html" class="nav-sub-link">Page Not Found</a>
+              <a href="page-500.html" class="nav-sub-link">Internal Server Error</a>
+              <a href="page-503.html" class="nav-sub-link">Service Unavailable</a>
+              <a href="page-505.html" class="nav-sub-link">Forbidden Access</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="user"></i> User Pages</a>
+            <nav class="nav nav-sub">
+              <a href="page-profile.html" class="nav-sub-link">User Profile</a>
+              <a href="page-timeline.html" class="nav-sub-link">Timeline</a>
+              <a href="page-people.html" class="nav-sub-link">People</a>
+              <a href="page-settings.html" class="nav-sub-link">Profile Settings</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="file-text"></i> Extras</a>
+            <nav class="nav nav-sub">
+              <a href="page-invoice.html" class="nav-sub-link">Invoice</a>
+              <a href="page-pricing.html" class="nav-sub-link">Pricing</a>
+            </nav>
+          </li>
+          <li class="nav-label"><label class="content-label">UI Design Library</label></li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="book"></i> Content</a>
+            <nav class="nav nav-sub">
+              <a href="../components/con-grid.html" class="nav-sub-link">Grid System</a>
+              <a href="../components/con-icons.html" class="nav-sub-link">Icons</a>
+              <a href="../components/con-images.html" class="nav-sub-link">Images</a>
+              <a href="../components/con-typography.html" class="nav-sub-link">Typography</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="layers"></i> Components</a>
+            <nav class="nav nav-sub">
+              <a href="../components/com-accordion.html" class="nav-sub-link">Accordion</a>
+              <a href="../components/com-alerts.html" class="nav-sub-link">Alerts</a>
+              <a href="../components/com-avatar.html" class="nav-sub-link">Avatar</a>
+              <a href="../components/com-badge.html" class="nav-sub-link">Badge</a>
+              <a href="../components/com-breadcrumbs.html" class="nav-sub-link">Breadcrumbs</a>
+              <a href="../components/com-buttons.html" class="nav-sub-link">Buttons</a>
+              <a href="../components/com-button-groups.html" class="nav-sub-link">Button Groups</a>
+              <a href="../components/com-cards.html" class="nav-sub-link">Cards</a>
+              <a href="../components/com-carousel.html" class="nav-sub-link">Carousel</a>
+              <a href="../components/com-collapse.html" class="nav-sub-link">Collapse</a>
+              <a href="../components/com-dropdown.html" class="nav-sub-link">Dropdown</a>
+              <a href="../components/com-list-group.html" class="nav-sub-link">List Group</a>
+              <a href="../components/com-marker.html" class="nav-sub-link">Marker</a>
+              <a href="../components/com-media-object.html" class="nav-sub-link">Media Object</a>
+              <a href="../components/com-modal.html" class="nav-sub-link">Modal</a>
+              <a href="../components/com-navs.html" class="nav-sub-link">Navs</a>
+              <a href="../components/com-navbar.html" class="nav-sub-link">Navbar</a>
+              <a href="../components/com-off-canvas.html" class="nav-sub-link">Off-Canvas</a>
+              <a href="../components/com-pagination.html" class="nav-sub-link">Pagination</a>
+              <a href="../components/com-placeholder.html" class="nav-sub-link">Placeholder</a>
+              <a href="../components/com-popovers.html" class="nav-sub-link">Popovers</a>
+                            <a href="../components/com-progress.html" class="nav-sub-link">Progress</a>
+              <a href="../components/com-steps.html" class="nav-sub-link">Steps</a>
+              <a href="../components/com-scrollbar.html" class="nav-sub-link">Scrollbar</a>
+              <a href="../components/com-scrollspy.html" class="nav-sub-link">Scrollspy</a>
+              <a href="../components/com-spinners.html" class="nav-sub-link">Spinners</a>
+              <a href="../components/com-tab.html" class="nav-sub-link">Tab</a>
+              <a href="../components/com-toast.html" class="nav-sub-link">Toast </a>
+              <a href="../components/com-tooltips.html" class="nav-sub-link">Tooltips</a>
+              <a href="../components/com-table-basic.html" class="nav-sub-link">Table Basic</a>
+              <a href="../components/com-table-advanced.html" class="nav-sub-link">Table (Datatable)</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="monitor"></i> Utilities</a>
+            <nav class="nav nav-sub">
+              <a href="../components/util-animation.html" class="nav-sub-link">Animation</a>
+              <a href="../components/util-background.html" class="nav-sub-link">Background</a>
+              <a href="../components/util-border.html" class="nav-sub-link">Border</a>
+              <a href="../components/util-display.html" class="nav-sub-link">Display</a>
+              <a href="../components/util-divider.html" class="nav-sub-link">Divider</a>
+              <a href="../components/util-flex.html" class="nav-sub-link">Flex</a>
+              <a href="../components/util-height.html" class="nav-sub-link">Height</a>
+              <a href="../components/util-margin.html" class="nav-sub-link">Margin</a>
+              <a href="../components/util-padding.html" class="nav-sub-link">Padding</a>
+              <a href="../components/util-position.html" class="nav-sub-link">Position</a>
+              <a href="../components/util-typography.html" class="nav-sub-link">Typography</a>
+              <a href="../components/util-width.html" class="nav-sub-link">Width</a>
+              <a href="../components/util-extras.html" class="nav-sub-link">Extras</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="life-buoy"></i> Forms</a>
+            <nav class="nav nav-sub">
+              <a href="../components/form-elements.html" class="nav-sub-link">Form Elements</a>
+              <a href="../components/form-input-group.html" class="nav-sub-link">Input Group</a>
+              <a href="../components/form-input-tags.html" class="nav-sub-link">Input Tags</a>
+              <a href="../components/form-input-masks.html" class="nav-sub-link">Input Masks</a>
+              <a href="../components/form-validation.html" class="nav-sub-link">Form Validation</a>
+              <a href="../components/form-wizard.html" class="nav-sub-link">Form Wizard</a>
+              <a href="../components/form-text-editor.html" class="nav-sub-link">Text Editor</a>
+              <a href="../components/form-rangeslider.html" class="nav-sub-link">Range Slider</a>
+              <a href="../components/form-datepicker.html" class="nav-sub-link">Date Pickers</a>
+              <a href="../components/form-select2.html" class="nav-sub-link">Select2</a>
+              <a href="../components/form-search.html" class="nav-sub-link">Search</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="pie-chart"></i> Charts</a>
+            <nav class="nav nav-sub">
+              <a href="../components/chart-flot.html" class="nav-sub-link">Flot</a>
+              <a href="../components/chart-chartjs.html" class="nav-sub-link">ChartJS</a>
+              <a href="../components/chart-peity.html" class="nav-sub-link">Peity</a>
+              <a href="../components/chart-sparkline.html" class="nav-sub-link">Sparkline</a>
+              <a href="../components/chart-morris.html" class="nav-sub-link">Morris</a>
+            </nav>
+          </li>
+          <li class="nav-item">
+            <a href="" class="nav-link with-sub"><i data-feather="map-pin"></i> Maps</a>
+            <nav class="nav nav-sub">
+              <a href="../components/map-google.html" class="nav-sub-link">Google Maps</a>
+              <a href="../components/map-leaflet.html" class="nav-sub-link">Leaflet Maps</a>
+              <a href="../components/map-vector.html" class="nav-sub-link">Vector Maps</a>
+            </nav>
+          </li>         
+        </ul>
+
+        <hr class="mg-t-30 mg-b-25">
+
+        <ul class="nav nav-sidebar">
+          <li class="nav-item"><a href="themes.html" class="nav-link"><i data-feather="aperture"></i> Themes</a></li>
+          <li class="nav-item"><a href="../docs.html" class="nav-link"><i data-feather="help-circle"></i> Documentation</a></li>
+        </ul>
+
+
+      </div><!-- sidebar-body -->
+    </div><!-- sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Frontend\TechnologyController;
 
 // backend
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\IndexPublicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -49,6 +50,18 @@ Route::name('technology.')->group(function () {
 });
 
 Route::get('wedding', [WeddingController::class, 'index']);
+Route::name('technology.')->group(function () {
+	Route::name('home.')->group(function () {
+		Route::get('frontend/home/create', [HomeController::class, 'create']);
+		Route::post('frontend/home/store', [HomeController::class, 'store']);
+		Route::get('frontend/home/show/{id}', [HomeController::class, 'show']);
+		Route::get('frontend/home/edit/{id}', [HomeController::class, 'edit']);
+		Route::post('frontend/home/update/{id}', [HomeController::class, 'update']);
+		Route::get('frontend/home/destroy/{id}', [HomeController::class, 'destroy']);
+	});		
+});
+
+Route::get('home-index', [IndexPublicController::class, 'index']);
 Route::name('technology.')->group(function () {
 	Route::name('home.')->group(function () {
 		Route::get('frontend/home/create', [HomeController::class, 'create']);
